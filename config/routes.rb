d688be837970d5479e0cab50866fbd472753aac1
Rutuja Deshkar <rutuja.deshkar@gmail.com>
2019-11-27 20:06:03 +0100

    Adds reviews on user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   root to: 'pages#home'
   resources :events do
     resources :bookings, only: [:show, :index, :new, :create, :update]
   end
   resources :bookings, only: [:destroy]
   resources :reviews, only: [:index, :new, :create]
-  resources :users, only: [:show]
   #resources :notifications, only: [:index, :new, :create]
 
   get '/mybookings',  to: 'dashboards#index'
   get '/myhostings',  to: 'dashboards#list'
+  get '/profile/:id',   to: 'profiles#show', as: :profile
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
