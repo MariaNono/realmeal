4f296f72ea69194a293a9041dce5883c5f3523cc
Maria Nogueira <leo.ds.nogueira@gmail.com>
2019-11-28 19:45:01 +0100

    make montserrat default font

--- app/javascript/pages/newbooking.js
+++ app/javascript/pages/newbooking.js
@@ -1,27 +1,27 @@
-// const numberOfGuests = document.querySelector('#booking_number_of_guests');
-// const pricePerGuest = document.querySelector("#price-per-guest")
-// const totalPriceGuest = document.querySelector("#total-price")
-// console.log(numberOfGuests);
-// console.log(pricePerGuest.innerText);
-// console.log(totalPriceGuest);
+const numberOfGuests = document.querySelector('#booking_number_of_guests');
+const pricePerGuest = document.querySelector("#price-per-guest")
+const totalPriceGuest = document.querySelector("#total-price")
+console.log(numberOfGuests);
+console.log(pricePerGuest.data);
+console.log(totalPriceGuest);
 
-// const calculatePrice = () => {
+const calculatePrice = () => {
 
-//     console.log("hi");
-//     // console.log(pricePerGuest.innerText.price);
-//     const price = parseFloat(pricePerGuest.dataset.price);
-//     const no_of_guests = numberOfGuests.value;
-//     const int_no_of_guests = parseInt(no_of_guests, 10)
-//     console.log(int_no_of_guests);
-//     const totalPrice = price * int_no_of_guests;
-//     totalPriceGuest.innerText = `Total price: ${parseFloat(totalPrice).toFixed(2)} €`;
-//   };
+    console.log("hi");
+    // console.log(pricePerGuest.innerText.price);
+    const price = parseFloat(pricePerGuest.dataset.price);
+    const no_of_guests = numberOfGuests.value;
+    const int_no_of_guests = parseInt(no_of_guests, 10)
+    console.log(int_no_of_guests);
+    const totalPrice = price * int_no_of_guests;
+    totalPriceGuest.innerText = `Total price: ${parseFloat(totalPrice).toFixed(2)} €`;
+  };
 
-// numberOfGuests.addEventListener('onChange', calculatePrice)
+numberOfGuests.addEventListener('onChange', calculatePrice)
 
 
 
-// export { calculatePrice };
+export { calculatePrice };
 
 // const reserveButton = getElementById('#reserve-button');
 // const message = getElementById('#message');
