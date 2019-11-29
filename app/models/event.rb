ce880d3f9569e36e1c20028a33066736f4444bf1
Rutuja Deshkar <rutuja.deshkar@gmail.com>
2019-11-29 10:50:06 +0100

    Minor fix for delete event and view all bookings

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   validates :name, :event_date, :description, :cuisine, :price_per_guest, :max_guests, :address, presence: true
   mount_uploader :photo, PhotoUploader
