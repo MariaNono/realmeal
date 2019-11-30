7b2fc6f4a028e7f76d6c57826ae85fa0d4f292fb
Maria Nogueira <leo.ds.nogueira@gmail.com>
2019-11-30 17:45:33 +0100

    improved my hostings, event bookings, my bookings, and put product name on the navbar, also made button colors consistent

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,6 @@
 require 'date'
 
 class DashboardsController < ApplicationController
-
   def index
     bookings = Booking.where(user_id: current_user)
     #@user_event_bookings = Booking.own_event_bookings(current_user)
@@ -16,18 +15,35 @@ class DashboardsController < ApplicationController
       end
     end
 
-    @upcoming_bookings = sort_by_date(upcoming_bookings)
-    @past_events = sort_by_date(past_events).reverse_each
+    @upcoming_bookings = sort_bookings_by_date(upcoming_bookings)
+    @past_events = sort_bookings_by_date(past_events).reverse_each
   end
 
   def list
-    @events = Event.where(user_id: current_user)
+    events = Event.where(user_id: current_user)
+
+    upcoming_hostings = []
+    past_hostings = []
+
+    events.each do |e|
+      if e.event_date < DateTime.now
+        past_hostings << e
+      else
+        upcoming_hostings << e
+      end
+    end
+
+    @upcoming_hostings = sort_hostings_by_date(upcoming_hostings)
+    @past_hostings = sort_hostings_by_date(past_hostings).reverse_each
   end
 
   private
 
-  def sort_by_date(bookings)
-    r = bookings.sort_by { |b| b.event.event_date }
+  def sort_bookings_by_date(bookings)
+    bookings.sort_by { |b| b.event.event_date }
   end
 
+  def sort_hostings_by_date(hostings)
+    hostings.sort_by { |e| e.event_date }
+  end
 end
